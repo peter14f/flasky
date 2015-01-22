2872e8208d0b89661fd760af8d8a8f962507a765
Peter Hsieh <peter14f@gmail.com>
2015-01-22 14:48:11 +0800

    added base template with bootstrap, 404.html and 500.html

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
     manager.run()
