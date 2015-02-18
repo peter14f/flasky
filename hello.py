0a859d221c43e527caebffdedec8478979f99d14
Peter Hsieh <peter14f@gmail.com>
2015-02-18 22:14:06 +0800

    added NameForm, now accepting inupt from user

--- hello.py
+++ hello.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route("/")
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("index.html", form=form, name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
