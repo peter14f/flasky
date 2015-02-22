1aadc27d164db34ce312f0d8166f461b1210251b
Peter Hsieh <peter14f@gmail.com>
2015-02-22 20:19:44 +0800

    fixed typo in errors.py

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_temlpate('404.html'), 404
+    return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
