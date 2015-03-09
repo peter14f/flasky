6dd802138285df724fed7fe57abb630a6b638ae4
Peter Hsieh <peter14f@gmail.com>
2015-03-09 23:12:08 +0800

    introduced a write-only 'password' column for 'User' table using property. Did basic testing on 'password'

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,7 @@
 import unittest
 from flask import current_app
 from app import create_app, db
+from app.models import User
 
 class BasisTestCase(unittest.TestCase):
     
@@ -22,4 +23,24 @@ class BasisTestCase(unittest.TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+class UserModelTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
+    
+    def test_no_password_getter(self):
+        u = User(password='cat')
+        with self.assertRaises(AttributeError):
+            u.password
+
+    def test_password_verification(self):
+        u = User(password='cat')
+        self.assertTrue(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('dog'))
+
+    def test_password_salts_are_random(self):
+        u1 = User(password='cat')
+        u2 = User(password='cat')
+        self.assertTrue(u1.password_hash != u2.password_hash)
 
