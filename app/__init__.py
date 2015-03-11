ac066f848a0102972935e8bf875ef40493d1d94e
Peter Hsieh <peter14f@gmail.com>
2015-03-11 23:26:28 +0800

    created login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask.ext.login import LoginManager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -9,11 +10,15 @@ bootstrap = Bootstrap()
 manager = Manager()
 db = SQLAlchemy()
 mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    login_manager.init_app(app)
     
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
