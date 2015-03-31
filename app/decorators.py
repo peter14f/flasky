8a309fa255ea0e62d36c0a122471da0bafa225c7
Peter Hsieh <peter14f@gmail.com>
2015-03-31 23:57:16 +0800

    Profiles editor

--- app/decorators.py
+++ app/decorators.py
@@ -1,9 +1,11 @@
 from functools import wraps
 from flask import abort, render_template
 from flask.ext.login import current_user
+from models import Permission
 
 def permission_required(permissions):
     def decorator(view_function):
+        @wraps(view_function)
         def wrapped_function(*args, **kwargs):
             if current_user.can(permissions):
                 response = view_function(*args, **kwargs)
@@ -13,6 +15,5 @@ def permission_required(permissions):
         return wrapped_function
     return decorator
 
-
-def admin_required(f):
-    return permission_required(Permission.ADMINISTER)(f)
+def admin_required(view_function):
+    return permission_required(Permission.ADMINISTER)(view_function)
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,13 @@
 from . import main
 from ..models import User
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, flash, abort
 from threading import Thread
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 from .. import db, mail
 from ..email import send_mail
 from ..models import Permission
 from ..decorators import permission_required, admin_required
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -42,3 +42,52 @@ def user(username):
 @permission_required(Permission.MODERATE_COMMENTS)
 def for_moderators_only():
     return "For comment moderators!"
+
+@main.route("/admin")
+@login_required
+@admin_required
+def for_admins_only():
+    return "For administrators!"
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user)
+    print form.user.email
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role_id = form.role.data
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash("Your changes on '" + user.username + "' have been made.")
+        return redirect(url_for('main.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me 
+    return render_template('edit_profile.html', form=form, user=user)
