da8f40437c3be11b637252b632829a057433eb03
Peter Hsieh <peter14f@gmail.com>
2015-04-05 17:32:24 +0800

    Generate fake users and posts

--- app/models.py
+++ app/models.py
@@ -72,6 +72,28 @@ class User(UserMixin, db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
     
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
+                     username=forgery_py.internet.user_name(True),
+                     password=forgery_py.lorem_ipsum.word(),
+                     confirmed=True,
+                     name=forgery_py.name.full_name(),
+                     location=forgery_py.address.city(),
+                     about_me=forgery_py.lorem_ipsum.sentence(),
+                     member_since=forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
     
@@ -168,6 +190,21 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count - 1)).first()
+            p = Post(body=forgery_py.forgery.lorem_ipsum.sentences(randint(1,3)),
+                     timestamp=forgery_py.forgery.date.date(True),
+                     author=u)
+            db.session.add(p)
+            db.session.commit()
 
 class Permission:
     FOLLOW = 0x01
