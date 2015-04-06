c00709a5c0d8629a0c1fcd0d41fda45c9ceaf02c
Peter Hsieh <peter14f@gmail.com>
2015-04-06 23:33:43 +0800

    Blog post pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from . import main
 from ..models import User, Post
-from flask import render_template, session, redirect, url_for, current_app, flash, abort
+from flask import render_template, session, redirect, url_for, current_app, flash, abort, request
 from threading import Thread
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db, mail
@@ -103,5 +103,9 @@ def index():
         new_post.author = current_user._get_current_object()
         db.session.add(new_post)
         return redirect(url_for("main.index"))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_POSTS_PER_PAGE = 15
     
     @staticmethod
     def init_app(app):
