3ca1e2ff765492582311a1c37e44d77dbc1c4473
Peter Hsieh <peter14f@gmail.com>
2015-04-07 23:22:24 +0800

    Permanent links to posts

--- app/main/views.py
+++ app/main/views.py
@@ -109,3 +109,8 @@ def index():
         error_out=False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts, pagination=pagination)
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
